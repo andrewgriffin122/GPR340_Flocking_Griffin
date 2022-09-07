b7f9b5c3c15dade33abee805d614d4264e0f20fe
Andrew Griffin <andrew.griffin@mymail.champlain.edu>
2022-09-06 20:19:37 -0400

    Starting work on alignment and cohesion rules

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -4,8 +4,24 @@
 Vector2 CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
     Vector2 cohesionForce;
 
+    Vector2 centerMass;
+    float dist;
+
     // todo: add your code here to make a force towards the center of mass
     // hint: iterate over the neighborhood
+    for (int i = 0; i < neighborhood.size(); i++)
+    {
+       //something something detection radius
+        dist = boid->getPosition().getDistance(boid->getPosition(), neighborhood[i]->getPosition());
+
+        if (dist <= boid->getDetectionRadius())
+        {
+            centerMass += neighborhood[i]->getPosition();
+        }
+    }
+    centerMass / neighborhood.size();
+    //this aint it bud
+    cohesionForce = centerMass - boid->getPosition();
 
     return cohesionForce;
 }
